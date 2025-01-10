2ccd4d82259b1167599526f9574c63a7d99c7220
DaniilProvotorov <dantcsar@gmail.com>
2025-01-10 17:28:05 +0500

    module_7_5.py

--- module_7_5.py
+++ module_7_5.py
@@ -4,10 +4,10 @@ import time
 directory = '.'
 
 for root, dirs, files in os.walk(directory):
-    for file in files:
-        file_path = os.path.join(os.getcwd())
-        file_time = os.path.getatime('module_7_4.py')
-        formatted_time = time.strftime('%d.%m.%Y %H:%M', time.localtime(file_time))
-        file_size = os.path.getsize('module_7_4.py')
-        parent_dir = os.path.dirname(file)
-        print(f'Найден файл:{file}, Путь:{file_path}, Размер файла:{file_size} байт, Время изменения:{formatted_time}, Родительская директория:{parent_dir} ')
+     for file in files:
+         file_path = os.path.join(directory, 'main.py')
+         file_time = os.path.getatime(file_path)
+         formatted_time = time.strftime('%d.%m.%Y %H:%M', time.localtime(file_time))
+         file_size = os.path.getsize(file_path)
+         parent_dir = os.path.dirname(file_path)
+         print(f'Найден файл:{file}, Путь:{file_path}, Размер файла:{file_size} байт, Время изменения:{formatted_time}, Родительская директория:{parent_dir} ')
